c1fd2e88b981351228a649c35404d627fecd38fb
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2023-03-29 11:16:12 -0300

    refactor: comic and character page data

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -4,39 +4,30 @@ import type { Character } from "~/types/character";
 
 const route = useRoute();
 
-const { data: character } = await useApi<Character>(
-  `/characters/${route.params.id}`,
-  {
-    transform(response: ApiResponse<Character[]>) {
-      const [value] = response.data.results;
-
-      return value;
-    },
-  }
+const { data } = await useApi<ApiResponse<Character[]>>(
+  `/characters/${route.params.id}`
 );
+
+const character = computed(() => data.value?.data.results[0]!);
 </script>
 
 <template>
-  <nuxt-layout>
-    <div class="py-6">
-      <div class="clear-both">
-        <div
-          class="w-2/5 float-left p-2 mr-10 border-2 border-red-500 shadow-xl"
-        >
-          <div class="pb-2/3 relative">
-            <app-thumbnail
-              :alt="character.name"
-              :thumbnail="character.thumbnail"
-              class="w-full h-full absolute inset-0"
-            />
-          </div>
-        </div>
-
-        <div class="flex flex-col gap-6">
-          <h1 class="text-4xl text-red-500">{{ character.name }}</h1>
-          <p class="text-xl text-justify">{{ character.description }}</p>
+  <div class="py-6">
+    <div class="clear-both">
+      <div class="w-2/5 float-left p-2 mr-10 border-2 border-red-500 shadow-xl">
+        <div class="pb-2/3 relative">
+          <app-thumbnail
+            :alt="character.name"
+            :thumbnail="character.thumbnail"
+            class="w-full h-full absolute inset-0"
+          />
         </div>
       </div>
+
+      <div class="flex flex-col gap-6">
+        <h1 class="text-4xl text-red-500">{{ character.name }}</h1>
+        <p class="text-xl text-justify">{{ character.description }}</p>
+      </div>
     </div>
-  </nuxt-layout>
+  </div>
 </template>
--- pages/comics/[id].vue
+++ pages/comics/[id].vue
@@ -4,34 +4,30 @@ import type { Comic } from "~/types/comic";
 
 const route = useRoute();
 
-const response = await useApi<ApiResponse<Comic[]>>(
+const { data } = await useApi<ApiResponse<Comic[]>>(
   `/comics/${route.params.id}`
 );
 
-const comic = computed(() => response.data.value?.data.results[0]!);
+const comic = computed(() => data.value?.data.results[0]!);
 </script>
 
 <template>
-  <nuxt-layout>
-    <div class="py-6">
-      <div class="clear-both">
-        <div
-          class="w-2/5 float-left p-2 mr-10 border-2 border-red-500 shadow-xl"
-        >
-          <div class="pb-2/3 relative">
-            <app-thumbnail
-              :alt="comic.title"
-              :thumbnail="comic.thumbnail"
-              class="w-full h-full absolute inset-0"
-            />
-          </div>
-        </div>
-
-        <div class="flex flex-col gap-6">
-          <h1 class="text-4xl text-red-500">{{ comic.title }}</h1>
-          <p class="text-xl text-justify">{{ comic.description }}</p>
+  <div class="py-6">
+    <div class="clear-both">
+      <div class="w-2/5 float-left p-2 mr-10 border-2 border-red-500 shadow-xl">
+        <div class="pb-2/3 relative">
+          <app-thumbnail
+            :alt="comic.title"
+            :thumbnail="comic.thumbnail"
+            class="w-full h-full absolute inset-0"
+          />
         </div>
       </div>
+
+      <div class="flex flex-col gap-6">
+        <h1 class="text-4xl text-red-500">{{ comic.title }}</h1>
+        <p class="text-xl text-justify">{{ comic.description }}</p>
+      </div>
     </div>
-  </nuxt-layout>
+  </div>
 </template>
