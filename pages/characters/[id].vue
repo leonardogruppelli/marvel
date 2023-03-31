74cfe91c07bb9947b2f525961d3776389dcf467d
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2023-03-31 15:37:02 -0300

    feat: step 8

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -1,15 +1,32 @@
+<script setup lang="ts">
+import type { IApiResponse } from "~/types/api";
+import type { ICharacter } from "~/types/character";
+
+const route = useRoute();
+
+const { data } = await useApi<IApiResponse<ICharacter[]>>(
+  `/characters/${route.params.id}`
+);
+
+const character = computed<ICharacter>(() => data.value?.data.results[0]!);
+</script>
+
 <template>
   <div class="py-10">
     <div class="clear-both">
       <div class="w-2/5 float-left p-2 mr-10 border-2 border-red-500 shadow-xl">
         <div class="pb-2/3 relative">
-          <app-thumbnail class="w-full h-full absolute inset-0" />
+          <app-thumbnail
+            :alt="character.name"
+            :thumbnail="character.thumbnail"
+            class="w-full h-full absolute inset-0"
+          />
         </div>
       </div>
 
       <div class="flex flex-col gap-6">
-        <h1 class="text-4xl text-red-500">Name</h1>
-        <p class="text-xl text-justify">Description</p>
+        <h1 class="text-4xl text-red-500">{{ character.name }}</h1>
+        <p class="text-xl text-justify">{{ trim(character.description) }}</p>
       </div>
     </div>
   </div>
